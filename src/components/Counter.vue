7d9ee925c571bb9834852ffdb25a1319d367fcdf
vishnuvs619 <vishnuvs241@gmail.com>
2020-07-25 17:42:17 +0530

    src/App.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+	<h2>The count is : {{count}}</h2>
+	<p>
+		<button @click="count+=1">Increment</button>
+	</p>
+	</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    	count: 0,
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
